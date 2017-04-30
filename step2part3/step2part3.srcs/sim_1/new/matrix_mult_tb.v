7f165fa32442778db3077c2a591bc1dba279a9f7
trakaros <caxelos@inf.uth.gr>
2017-04-30 15:16:29 +0300

    apo1024se10

--- step2part3/step2part3.srcs/sim_1/new/matrix_mult_tb.v
+++ step2part3/step2part3.srcs/sim_1/new/matrix_mult_tb.v
@@ -20,40 +20,62 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 module matrix_mult_tb;
-  parameter N = 32;
-  wire [N-1:0] X, Y;
-  wire [31 : 0] Ymem [0 : 1023];
-  reg counter;
-  matrix_multiplication matrix_mult_instance(X, Ymem);
 
+
+
+  parameter N = 32;
+ 
+  wire [10*32-1 : 0] memOut;//wire [1024*32-1 : 0] memOut ;
+  wire [9:0] size;
+  wire ENABLE;
+  
+  reg [3:0] counter;//reg [9:0] counter;//10 = log2(1024)
+  reg [N-1:0] X;
+  reg TRIGGER;
+  reg clk, reset;
+ 
+  
+  assign size = 4'd10;//10'd1024;
+    
   initial begin
     reset = 1;
     clk = 0;
     
-    #100 reset = 0;  
+    #1000 reset = 0;  
   end
 
+  //edw arxikopoiw ton disdiastato pinaka pou einai na xrisimopoihsoume
   
-  for (i = 0; i < 
+  always @(posedge clk)  begin
+    if (reset) begin
+      counter <= 0;      
+      TRIGGER <= 0; 
+      X[3:0] <= size;//X[9:0] <= size; 
+      X[31:10] <= 0;
+    end
+    else begin
+      if (counter == size) begin
+        TRIGGER <= 1;
+        //counter <= 0 ;           
+      end
+      else  begin
+        X[31:0] <= {22'b0, counter};
+        counter <= counter + 1; 
+      end  
+    end
+  end
+
+matrix_multiplication matrix_mult_instance(
+  .clk(clk), 
+  .reset(reset), 
+  .TRIGGER(TRIGGER),  
+  .X(X), 
+  .ENABLE(ENABLE), 
+  .memOut(memOut) 
+);
   
   always
     #10 clk <= ~clk; 
 
-  always @(posedge clk)  begin
-    if (reset) begin
-      trigger <= 0;
-      counter <= 1;
-      size <= 1024;  
-    end
-    else begin
-      if (counter == size)
-      
-      end
-      else  begin
-        counter <= counter + 1; 
-      end
-    
-    end
-  end
 
 endmodule
