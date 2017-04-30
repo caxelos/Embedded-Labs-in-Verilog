af692926bf4c1d05c74d358d2e8915944675fc26
trakaros <caxelos@inf.uth.gr>
2017-04-30 15:12:50 +0300

    kapws kalutera

--- step2part3/step2part3.srcs/sources_1/new/matrix_multiplication.v
+++ step2part3/step2part3.srcs/sources_1/new/matrix_multiplication.v
@@ -18,68 +18,87 @@
 // Additional Comments:
 // 
 //////////////////////////////////////////////////////////////////////////////////
-parameter A = 2;
-parameter N = 32;
-parameter RESET = 0;
-parameter RECEIVE_SIZE = 1;
-parameter RECEIVE_INPUT = 2;
-parameter INPUT_STORED = 3;
-parameter OUTPUT_STORED = 4;
-parameter OUTPUT_SEND = 5;
+
 //1024 = 32^2;
 module matrix_multiplication(
   input clk,
   input reset,
   input TRIGGER,
-  input wire [N-1:0] X, 
+  input wire [31:0] X, 
   //output wire [N-1:0] Y
-  output reg [31 : 0] Ymem [0 : 1023]
+ // output wire [N-1 : 0] Ymem [0 : 1023]
+  output reg ENABLE, 
+  //output wire [1024*32-1 : 0] memOut
+  output wire [10*32-1 : 0] memOut
 );
-
-//32x32 pinakas kathe stoixeio 32bits
-  reg [31 : 0] Xmem [0 : 1023];
+  parameter A = 2;
   
-  reg [31 : 0] counter, size;// 0 until 1023 so we need log(1024) = 32;
-  reg [2:0] state;
-  reg ENABLE;
+  parameter RECEIVE_SIZE = 0;
+  parameter RECEIVE_INPUT = 1;
+  parameter INPUT_STORED = 2;
+  parameter OUTPUT_STORED = 3;
+  parameter OUTPUT_SEND = 4;
+  
+  
+  reg [31 : 0] Ymem [0 : 9];//Ymem [0 : 1023];
+  reg [31:0] Xmem [0 : 9];//[0 : 1023];
+  reg [3:0] counter, size;//reg [9: 0] counter, size;// 0 until 1023 so we need log(1024) = 32;
+  reg [1:0] state;
   // afto einai gia anathesi sta Xmem molis energopoihthei to sima
   // trigger
   
+  
+  genvar i; generate
+  //for (i = 1; i <= 1024; i = i + 1) begin: YmemOut
+  for (i = 1; i <= 10; i = i + 1) begin: YmemOut
+    assign memOut[i*32-1:(i-1)*32] = Ymem[i-1];
+  end
+  endgenerate
+  
   always @(posedge clk)  begin
-    if (state == RESET) begin
-      size <= X;
+    //if (state == RESET) begin
+    if (reset) begin
+      size <= 0;
+      counter <= 0;
+      ENABLE <= 0;
+      
+      //state <= RECEIVE_INPUT;//RECEIVE_SIZE;
       state <= RECEIVE_SIZE;
-      ENABLE <= 0;
     end
-    else if (state == RECEIVE_SIZE) begin
-      state <= RECEIVE_INPUT;
-    end
-    else if (state == RECEIVE_INPUT) begin
-      if (TRIGGER) begin  
-        state <= INPUT_STORED;
-        counter <= 0;
-      end  
-      else begin
-        Xmem[counter] <= X;
-        counter <= counter + 1;
-      end  
-    end
-    else if (state == INPUT_STORED) begin//counter = counter + 1
-      if (counter == size) begin
-        state <= OUTPUT_STORED;
-        ENABLE <= 1;
-        counter <= 0;
+    else begin    
+      if (state == RECEIVE_SIZE) begin
+        size <= X[3:0];//size <= X[9:0];
+        state <= RECEIVE_INPUT;
+      end   
+      else if (state == RECEIVE_INPUT) begin
+       /*
+       arxika stelnoume to size, meta to data kai meta
+       stelnoume to trigger gia na dilwsoume oti teleiwse i apostoli
+       */
+          
+        if (TRIGGER) begin // mporeis na arxiseis na ekteleis tis prakseis 
+          state <= INPUT_STORED;
+          counter <= 0;
+        end  
+        else begin
+          Xmem[counter] <= X;  
+          counter <= counter + 1;  
+        end
       end
-      else begin
-        Ymem[counter] <= A * Xmem[counter] * Xmem[counter];       
-        counter <= counter + 1;
+      else if (state == INPUT_STORED) begin//counter = counter + 1
+        if (counter == size) begin //molis oloklirwthoun oi upologismoi
+          state <= OUTPUT_STORED;
+          ENABLE <= 1;
+          counter <= 0;
+        end
+        else begin//kane tis prakseis
+          Ymem[counter] <= A * Xmem[counter] * Xmem[counter];       
+          counter <= counter + 1;
+        end
       end
-    end
-    else if (state == OUTPUT_STORED) begin
-      //state = OUTPUT_SEND;
-      state <= RESET;
-      ENABLE <= 0;
+      else if (state == OUTPUT_STORED) begin
+        ENABLE <= 0;//an i diadikasia apostolis twn data diarkei 1 kuklo eimaste OK alliws problima
+      end
     end
   end
-  
 endmodule
