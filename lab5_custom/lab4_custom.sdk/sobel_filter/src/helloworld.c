52eec90d35a942c9daaaaa022edecf22ddcd749c
trakaros <caxelos@inf.uth.gr>
2017-06-19 23:21:59 +0300

    epitelous bitstream vivado!

--- lab5_custom/lab4_custom.sdk/sobel_filter/src/helloworld.c
+++ lab5_custom/lab4_custom.sdk/sobel_filter/src/helloworld.c
@@ -1,258 +1,102 @@
-#include <stdio.h>
-#include "platform.h"
 #include <math.h>
-#include <string.h>
 #include <stdlib.h>
-#include <time.h>
-#include <errno.h>
-#include "xsdps.h"
-#include "ff.h"
-#include "xil_io.h"
-#include "xil_types.h"
-#include "xtmrctr.h"
-#include "xscutimer.h"
-#include <sys/time.h>
 
-//timer info
-#define TIMER_DEVICE_ID		XPAR_XSCUTIMER_0_DEVICE_ID
-#define INTC_DEVICE_ID		XPAR_SCUGIC_SINGLE_DEVICE_ID
-#define TIMER_IRPT_INTR		XPAR_SCUTIMER_INTR
-#define TIMER_LOAD_VALUE	0xFFFFFFFF
-XScuTimer	Timer;
+#define SIZE 50
 
-#define SIZE	1024
-
-static char INPUT_FILE[32] = "input.gre";
-static char OUTPUT_FILE[32] = "output.gre";
-static char GOLDEN_FILE[32] = "golden.gre";
-static char *Log_File;
-
-static FIL file1,file2,file3;
-/* The horizontal and vertical operators to be used in the sobel filter */
-//Use -fsigned-char flag in order to use signed chars
-int horiz_operator[3][3] = {{-1, 0, 1},
-                             {-2, 0, 2},
-                             {-1, 0, 1}};
-int vert_operator[3][3] = {{1, 2, 1},
-                            {0, 0, 0},
-                            {-1, -2, -1}};
-
-double sobel(unsigned char *input, unsigned char *output, unsigned char *golden);
-int convolution2D(int posy, int posx, const unsigned char *input, int operator[][3]);
-
-/* The arrays holding the input image, the output image and the output used *
- * as golden standard. The luminosity (intensity) of each pixel in the      *
- * grayscale image is represented by a value between 0 and 255 (an unsigned *
- * character). The arrays (and the files) contain these values in row-major *
- * order (element after element within each row and row after row. 			*/
-unsigned char input[SIZE*SIZE], output[SIZE*SIZE], golden[SIZE*SIZE];
+void ip_sobel(unsigned char input_img[SIZE*SIZE], unsigned char output_img[SIZE*SIZE]);//, unsigned char golden);
+//int convolution2D(int posy, int posx, const unsigned char input_img[SIZE*SIZE], int operator[3][3]);
 
 
-/* Implement a 2D convolution of the matrix with the operator */
-/* posy and posx correspond to the vertical and horizontal disposition of the *
- * pixel we process in the original image, input is the input image and       *
- * operator the operator we apply (horizontal or vertical). The function ret. *
- * value is the convolution of the operator with the neighboring pixels of the*
- * pixel we process.														  */
-int convolution2D(int posy, int posx, const unsigned char *input, int operator[][3]) {
-	int i, j, res;
 
-	res = 0;
-	for (j = -1; j <= 1; j++) {
-		for (i = -1; i <= 1; i++) {
-			res += input[(posy + i)*SIZE + posx + j] * operator[i+1][j+1];
+static  int horiz_operator[3][3] = {      {-1, 0, 1},
+    	                                     {-2, 0, 2},
+    	                                     {-1, 0, 1}};
+static  int vert_operator[3][3] =        {{1, 2, 1},
+    	                                     {0, 0, 0},
+    	                                    {-1, -2, -1}};
+
+
+/*
+int convolution2D(int posy, int posx,  const unsigned char input_img[SIZE*SIZE], int operator[3][3]) {
+
+	int i=0, j=0, res=0;
+	//#pragma AP PIPELINE...3*3*2 accesses. 3*3*6 adders, 3*3*2 mults
+
+	convolution2D_label1:for (j = -1; j <= 1; j++) {
+		convolution2D_label0:for (i = -1; i <= 1; i++) {
+			res += input_img[(posy + i)*SIZE + posx + j] * operator[i+1][j+1];
 		}
 	}
-	return(res);
+
+
+
+
+	return( res);
 }
-
-
-/* The main computational function of the program. The input, output and *
- * golden arguments are pointers to the arrays used to store the input   *
- * image, the output produced by the algorithm and the output used as    *
- * golden standard for the comparisons.									 */
-double sobel(unsigned char *input, unsigned char *output, unsigned char *golden)
+*/
+void ip_sobel(unsigned char input_img[SIZE*SIZE], unsigned char output_img[SIZE*SIZE])//, unsigned char *golden)
 {
-	double PSNR = 0, t;
-	int i, j;
-	unsigned int p;
-	int res;
-	FRESULT f_in, f_out, f_golden;
 
-	XScuTimer_Config *TMRConfigPtr;     //timer config
+	int i, j, k, l;
+	unsigned int p=0, resHoriz=0, resVert=0;
+	int res=0;
+    int one=0, two=0;
 
-	TMRConfigPtr = XScuTimer_LookupConfig(TIMER_DEVICE_ID);
-	XScuTimer_CfgInitialize(&Timer, TMRConfigPtr,TMRConfigPtr->BaseAddr);
-	XScuTimer_SelfTest(&Timer);
-	//load the timer
-	XScuTimer_LoadTimer(&Timer, TIMER_LOAD_VALUE);
-	int tick;
-	/* The first and last row of the output array, as well as the first  *
-     * and last element of each column are not going to be filled by the *
-     * algorithm, therefore make sure to initialize them with 0s.		 */
-	memset(output, 0, SIZE*sizeof(unsigned char));
-	memset(&output[SIZE*(SIZE-1)], 0, SIZE*sizeof(unsigned char));
-	for (i = 1; i < SIZE-1; i++) {
-		output[i*SIZE] = 0;
-		output[i*SIZE + SIZE - 1] = 0;
-	}
-
-	int zeros=0;
-
-	for(int i=0;i<SIZE*SIZE;i++){
-		if(output[i]==0){
-			zeros++;
-		}
-	}
-
-	printf("Output initialy has %d zeros\n",zeros);
-	/* Open the input, output, golden files, read the input and golden    *
-     * and store them to the corresponding arrays.						  */
-
-	Log_File = (char *)INPUT_FILE;
-	f_in = f_open(&file1, Log_File,FA_READ);
-	if (f_in!= FR_OK) {
-		printf("File  INPUT_FILE  not found\n");
-		return XST_FAILURE;
-	}
-	Log_File = (char *)GOLDEN_FILE;
-	f_golden = f_open(&file2, Log_File,FA_READ);
-	if (f_golden!= FR_OK) {
-		printf("File  GOLDEN_FILE  not found\n");
-		return XST_FAILURE;
-	}
-
-	Log_File = (char *)OUTPUT_FILE;
-	f_out = f_open(&file3, Log_File, FA_CREATE_ALWAYS | FA_WRITE);
-	if (f_out!= FR_OK) {
-		printf("File  OUTPUT_FILE  not found\n");
-		return XST_FAILURE;
-	}
+    int rowOffset, colOffset, x_dir, y_dir, edge_weight;
 
 
-	uint readBytes=SIZE*SIZE;
-	int off =0;
-	f_read(&file1,&input[off],SIZE*SIZE,&readBytes);
-	off+=readBytes;
-	printf("readbytes = %d\n",readBytes);
+    ip_sobel_label8:for(i = 0; i < SIZE; i++){
+        	ip_sobel_label9:for(j=0; j < SIZE; j++){
+        		x_dir = 0;
+        		y_dir = 0;
+        		if (i > 0 && (i < (SIZE-1)) && (j > 0) &&   (j < (SIZE-1)) ){
+        			ip_sobel_label7:for(rowOffset = -1; rowOffset <= 1; rowOffset++){
+        				ip_sobel_label6:for(colOffset = -1; colOffset <=1; colOffset++){
+        					x_dir = x_dir + input_img[(i+rowOffset)*SIZE +j+colOffset]* horiz_operator[1+rowOffset][1+colOffset];
+        					y_dir = y_dir + input_img[(i+rowOffset)*SIZE +j+colOffset]* vert_operator[1+rowOffset][1+colOffset];
+        				}
+        			}
+        			edge_weight = abs(x_dir) + abs(y_dir);
+        			output_img[i*SIZE +j] = edge_weight;
+        		}
+        	}
+        }
 
 
-	uint readBytes2=SIZE*SIZE;
-	off =0;
-	f_read(&file2,&golden[off],SIZE*SIZE,&readBytes2);
-	off+=readBytes2;
-
-	printf("Just read from golden with start %p\n",&golden[0]);
-	printf("Read:%d bytes\n",off);
-
-	f_close(&file1);
-	f_close(&file2);
-	int one=0,two=0;
-
-  	printf("Started\n");
-    printf("Start timer\r\n");
-    XScuTimer_Start(&Timer);
-
+    /*
+    			p = (convolution2D(i, j, input_img, horiz_operator))*(convolution2D(i, j, input_img, horiz_operator)) +
+    				(convolution2D(i, j, input_img, vert_operator))*(convolution2D(i, j, input_img, vert_operator));
+    			*/
+/******* S T A R T  ***********************************************************************/
 	/* This is the main computation. Get the starting time. */
 	/* For each pixel of the output image */
-	for (j=1; j<SIZE-1; j+=1) {
-		for (i=1; i<SIZE-1; i+=1 ) {
-			/* Apply the sobel filter and calculate the magnitude *
-			 * of the derivative.								  */
-			p = (convolution2D(i, j, input, horiz_operator))*(convolution2D(i, j, input, horiz_operator)) +
-				(convolution2D(i, j, input, vert_operator))*(convolution2D(i, j, input, vert_operator));
+   /*
+	ip_sobel_label5:for (j=1; j<SIZE-1; j+=1) {//unroll
+		ip_sobel_label0:for (i=1; i<SIZE-1; i+=1 ) {//dataflow
+
+			ip_sobel_label4:for (k = -1; k <= 1; k++) {
+				ip_sobel_label3:for (l = -1; l <= 1; l++) {
+					resHoriz += input_img[(i + l)*SIZE + j + k] * horiz_operator[l+1][k+1];
+				//}
+			//}
+			//ip_sobel_label2:for (k = -1; k <= 1; k++) {//unroll
+				//ip_sobel_label1:for (l = -1; l <= 1; l++) {//pipeline
+					resVert += input_img[(i + l)*SIZE + j + k] * vert_operator[l+1][k+1];
+				}
+			}
+
 			res = (int) sqrt((double) p);
 			if (res > 255){
-				output[i*SIZE + j] = 255;
+				output_img[i*SIZE + j] = 255;
 				one++;
 			}
 			else{
-				output[i*SIZE + j] = (unsigned char)res;
+				output_img[i*SIZE + j] = (unsigned char)res;
 				two++;
 			}
+
 		}
 	}
-	printf("Ended\n");
-	XScuTimer_Stop(&Timer);
-	printf("Measure timer\r\n");
-
-	printf("Measured\r\n");
-
-	tick = XScuTimer_GetCounterValue(&Timer);
-
-	printf("Ended with %.6lf secs\r\n",(double)(-1*tick)/XPAR_PS7_CORTEXA9_0_CPU_CLK_FREQ_HZ);
-
-	printf("Sobel done with ones:%d and twos:%d\n",one,two);
-
-	/* Now run through the output and the golden output to calculate *
-	 * the MSE and then the PSNR.									 */
-	for (i=1; i<SIZE-1; i++) {
-		for ( j=1; j<SIZE-1; j++ ) {
-			t = pow((output[i*SIZE+j] - golden[i*SIZE+j]),2);
-			PSNR += t;
-		}
-	}
-
-	PSNR /= (double)(SIZE*SIZE);
-	PSNR = 10*log10(65536/PSNR);
-	printf("PSNR:%g\n", PSNR);
-
-	/* This is the end of the main computation. Take the end time,  *
-	 * calculate the duration of the computation and report it. 	*/
-
-	/* Write the output file */
-	res = f_lseek(&file3, 0);
-	if (res!= FR_OK) {
-		return XST_FAILURE;
-	}
-	off =0;
-	uint writtenBytes=0;
-	while(writtenBytes!=SIZE*SIZE) {
-		f_out = f_write(&file3,&output[off],SIZE*SIZE,&writtenBytes);
-		if (f_out!=0) {
-			xil_printf(" ERROR: f_write2 returned %d\r\n",f_out);
-			//return XST_FAILURE;
-		}
-		off+=writtenBytes;
-
-	}
-
-	if (f_out!=FR_OK) {
-			xil_printf(" ERROR: f_write2 returned %d\r\n",f_out);
-
-			return XST_FAILURE;
-	}
-	off+=writtenBytes;
-
-	printf("Just written to output with start %p\n",&output[0]);
-	printf("Written:%d bytes\n",writtenBytes);
-
-	f_close(&file3);
-
-	return PSNR;
-}
-
-
-
-int main(int argc, char* argv[])
-{
-
-    init_platform();
-
-    static FATFS  FS_instance;
-    const char *Path = "0:/";
-	FRESULT  result;
-	result = f_mount(&FS_instance,Path, 0);
-	if (result != FR_OK) {
-		printf("Cannot mount sd\n");
-		return XST_FAILURE;
-	}
-	printf("Sobel filter start!\n");
-	double PSNR;
-	PSNR = sobel(input, output, golden);
-	printf("PSNR of original Sobel and computed Sobel image: %g\n", PSNR);
-	printf("Create the output image with:$convert -depth 8 -size 1024x1024 GRAY:output_sobel.grey output_sobel.jpg\n");
-    cleanup_platform();
-
-	return 0;
+	*/
+	//return PSNR;
 }
